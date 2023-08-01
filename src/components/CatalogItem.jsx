ac49e0bc1d6450c5175b154344d087a73102445d
adriansyah <adriansyah.azhari@kelaspintar.id>
2023-08-01 23:36:24 +0700

    adrian contoh

--- src/components/CatalogItem.jsx
+++ src/components/CatalogItem.jsx
@@ -3,100 +3,262 @@ import logo from "../img/background hero.jpg";
 
 function CatalogItem() {
     return (
-        <div class="container border border-dark border-1 p-0">
-            <div class="row my-3">
-                <div class="col-4 col-sm-2">
-                    <div
-                        class="card my-2 shadow_card bgc_card"
-                        style={{ width: "11rem" }}
-                    >
-                        <img src={logo} class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Rp 199.000</p>
-                            <a href="#" class="btn">
-                                Beli Sekarang
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4 col-sm-2">
-                    <div
-                        class="card my-2 shadow_card bgc_card"
-                        style={{ width: "11rem" }}
-                    >
-                        <img src={logo} class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Rp 199.000</p>
-                            <a href="#" class="btn">
-                                Beli Sekarang
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4 col-sm-2">
-                    <div
-                        class="card my-2 shadow_card bgc_card"
-                        style={{ width: "11rem" }}
-                    >
-                        <img src={logo} class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Rp 199.000</p>
-                            <a href="#" class="btn">
-                                Beli Sekarang
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4 col-sm-2">
-                    <div
-                        class="card my-2 shadow_card bgc_card"
-                        style={{ width: "11rem" }}
-                    >
-                        <img src={logo} class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Rp 199.000</p>
-                            <a href="#" class="btn">
-                                Beli Sekarang
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4 col-sm-2">
-                    <div
-                        class="card my-2 shadow_card bgc_card"
-                        style={{ width: "11rem" }}
-                    >
-                        <img src={logo} class="card-img-top" alt="hero" />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Rp 199.000</p>
-                            <a href="#" class="btn">
-                                Beli Sekarang
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4 col-sm-2">
-                    <div
-                        class="card my-2 shadow_card bgc_card"
-                        style={{ width: "11rem" }}
-                    >
-                        <img src={logo} class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Rp 199.000</p>
-                            <a href="#" class="btn">
-                                Beli Sekarang
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        // <div class="container border border-dark border-1 p-0">
+        //     <div class="row my-3">
+        //         <div class="col-4 col-sm-2">
+        //             <div
+        //                 class="card my-2 shadow_card bgc_card"
+        //                 style={{ width: "11rem" }}
+        //             >
+        //                 <img src={logo} class="card-img-top" alt="..." />
+        //                 <div class="card-body">
+        //                     <h5 class="card-title">Card title</h5>
+        //                     <p class="card-text">Rp 199.000</p>
+        //                     <a href="#" class="btn">
+        //                         Beli Sekarang
+        //                     </a>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <div class="col-4 col-sm-2">
+        //             <div
+        //                 class="card my-2 shadow_card bgc_card"
+        //                 style={{ width: "11rem" }}
+        //             >
+        //                 <img src={logo} class="card-img-top" alt="..." />
+        //                 <div class="card-body">
+        //                     <h5 class="card-title">Card title</h5>
+        //                     <p class="card-text">Rp 199.000</p>
+        //                     <a href="#" class="btn">
+        //                         Beli Sekarang
+        //                     </a>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <div class="col-4 col-sm-2">
+        //             <div
+        //                 class="card my-2 shadow_card bgc_card"
+        //                 style={{ width: "11rem" }}
+        //             >
+        //                 <img src={logo} class="card-img-top" alt="..." />
+        //                 <div class="card-body">
+        //                     <h5 class="card-title">Card title</h5>
+        //                     <p class="card-text">Rp 199.000</p>
+        //                     <a href="#" class="btn">
+        //                         Beli Sekarang
+        //                     </a>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <div class="col-4 col-sm-2">
+        //             <div
+        //                 class="card my-2 shadow_card bgc_card"
+        //                 style={{ width: "11rem" }}
+        //             >
+        //                 <img src={logo} class="card-img-top" alt="..." />
+        //                 <div class="card-body">
+        //                     <h5 class="card-title">Card title</h5>
+        //                     <p class="card-text">Rp 199.000</p>
+        //                     <a href="#" class="btn">
+        //                         Beli Sekarang
+        //                     </a>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <div class="col-4 col-sm-2">
+        //             <div
+        //                 class="card my-2 shadow_card bgc_card"
+        //                 style={{ width: "11rem" }}
+        //             >
+        //                 <img src={logo} class="card-img-top" alt="hero" />
+        //                 <div class="card-body">
+        //                     <h5 class="card-title">Card title</h5>
+        //                     <p class="card-text">Rp 199.000</p>
+        //                     <a href="#" class="btn">
+        //                         Beli Sekarang
+        //                     </a>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <div class="col-4 col-sm-2">
+        //             <div
+        //                 class="card my-2 shadow_card bgc_card"
+        //                 style={{ width: "11rem" }}
+        //             >
+        //                 <img src={logo} class="card-img-top" alt="..." />
+        //                 <div class="card-body">
+        //                     <h5 class="card-title">Card title</h5>
+        //                     <p class="card-text">Rp 199.000</p>
+        //                     <a href="#" class="btn">
+        //                         Beli Sekarang
+        //                     </a>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+        
+
+        <div class="container">
+    <div className="row justify-content-center">
+        <div className="col my-4">
+        <div class="card">
+  <div class="card-body">
+    This is some text within a card body.
+  </div>
+</div>
         </div>
+        <div className="col my-4">
+        <div class="card">
+  <div class="card-body">
+    This is some text within a card body.
+  </div>
+</div>
+        </div>
+        <div className="col my-4">
+        <div class="card">
+  <div class="card-body">
+    This is some text within a card body.
+  </div>
+</div>
+        </div>
+    </div>
+
+  <div class="row align-items-start" > 
+    <div class="col">
+    <div
+                        class="card my-2 shadow_card bgc_card"
+                    >
+                        <img src={logo} class="card-img-top img-fluid " alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Rp 199.000</p>
+                            <a href="#" class="btn">
+                                Beli Sekarang
+                            </a>
+                        </div>
+                    </div>
+    </div>
+    <div class="col">
+    <div
+                        class="card my-2 shadow_card bgc_card"
+                    >
+                        <img src={logo} class="card-img-top img-fluid" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Rp 199.000</p>
+                            <a href="#" class="btn">
+                                Beli Sekarang
+                            </a>
+                        </div>
+                    </div>
+    </div>
+    <div class="col">
+    <div
+                        class="card my-2 shadow_card bgc_card"
+                    >
+                        <img src={logo} class="card-img-top img-fluid" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Rp 199.000</p>
+                            <a href="#" class="btn">
+                                Beli Sekarang
+                            </a>
+                        </div>
+                    </div>
+    </div>
+  
+    <div class="col">
+    <div
+                        class="card my-2 shadow_card bgc_card"
+                    >
+                        <img src={logo} class="card-img-top img-fluid" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Rp 199.000</p>
+                            <a href="#" class="btn">
+                                Beli Sekarang
+                            </a>
+                        </div>
+                    </div>
+    </div>
+    <div class="col">
+    <div
+                        class="card my-2 shadow_card bgc_card"
+                    >
+                        <img src={logo} class="card-img-top img-fluid" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Rp 199.000</p>
+                            <a href="#" class="btn">
+                                Beli Sekarang
+                            </a>
+                        </div>
+                    </div>
+    </div>
+    <div class="col">
+    <div
+                        class="card my-2 shadow_card bgc_card"
+                    >
+                        <img src={logo} class="card-img-top img-fluid" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Rp 199.000</p>
+                            <a href="#" class="btn">
+                                Beli Sekarang
+                            </a>
+                        </div>
+                    </div>
+    </div>
+  
+  
+    <div class="col">
+    <div
+                        class="card my-2 shadow_card bgc_card"
+                    >
+                        <img src={logo} class="card-img-top img-fluid" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Rp 199.000</p>
+                            <a href="#" class="btn">
+                                Beli Sekarang
+                            </a>
+                        </div>
+                    </div>
+    </div>
+    <div class="col">
+      <div
+                        class="card my-2 shadow_card bgc_card"
+                    >
+                        <img src={logo} class="card-img-top img-fluid" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Rp 199.000</p>
+                            <a href="#" class="btn">
+                                Beli Sekarang
+                            </a>
+                        </div>
+                    </div>
+    </div>
+    <div class="col">
+      <div
+                        class="card my-2 shadow_card bgc_card"
+                    >
+                        <img src={logo} class="card-img-top img-fluid" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Rp 199.000</p>
+                            <a href="#" class="btn">
+                                Beli Sekarang
+                            </a>
+                        </div>
+                    </div>
+    </div>
+  </div>
+
+  </div>
+
     );
 }
 
